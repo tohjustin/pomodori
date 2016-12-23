bd8540b1305178c51f74835996db980a37d8b96b
Justin Toh <tohjustin@hotmail.com>
2016-12-22 17:13:51 -0800

    Added unit tests for [radialBar] component

---
+++ test/unit/specs/radialBar.spec.js
@@ -0,0 +1,98 @@
+import Vue from 'vue'
+import { mountComponent, destroyVueInstance } from '../utils.js'
+import radialBar from 'src/components/radialBar.vue'
+
+describe('radialBar', () => {
+  let vm = null
+
+  afterEach(() => {
+    destroyVueInstance(vm)
+  })
+
+  it('should render correct contents', () => {
+    vm = mountComponent(radialBar, {
+      fraction: '0',
+      overlayText: '00:00',
+      strokeColor: '#2196F3', // rgb(33, 150, 243)
+      trailColor: '#ABABAB' // rgb(171, 171, 171)
+    })
+    expect(vm.$el.querySelector('.radialBar-overlay').textContent)
+      .to.equal('00:00')
+    expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+      .to.equal('linear-gradient(270deg, rgb(171, 171, 171) 50%, transparent 50%, transparent), linear-gradient(270deg, rgb(33, 150, 243) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+  })
+
+  it('should update contents correctly after changing [overlayText] props', done => {
+    vm = mountComponent(radialBar, {
+      fraction: '0',
+      overlayText: '00:00',
+      strokeColor: '#2196F3', // rgb(33, 150, 243)
+      trailColor: '#ABABAB' // rgb(171, 171, 171)
+    })
+
+    // Set overlayText to "23:59"
+    vm.overlayText = '23:59'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-overlay').textContent)
+        .to.equal('23:59')
+      done()
+    })
+  })
+
+  it('should update contents correctly after changing [fraction] props', done => {
+    vm = mountComponent(radialBar, {
+      fraction: '0',
+      overlayText: '00:00',
+      strokeColor: '#2196F3', // rgb(33, 150, 243)
+      trailColor: '#ABABAB' // rgb(171, 171, 171)
+    })
+
+    // Set radial-bar to 25% or 0.25
+    vm.fraction = '0.25'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+        .to.equal('linear-gradient(270deg, rgb(171, 171, 171) 50%, transparent 50%, transparent), linear-gradient(rgb(33, 150, 243) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+      done()
+    })
+
+    // Set radial-bar to 50% or 0.50
+    vm.fraction = '0.25'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+        .to.equal('linear-gradient(270deg, rgb(171, 171, 171) 50%, transparent 50%, transparent), linear-gradient(90deg, rgb(33, 150, 243) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+      done()
+    })
+
+    // Set radial-bar to 75% or 0.75
+    vm.fraction = '0.25'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+        .to.equal('linear-gradient(0deg, rgb(33, 150, 243) 50%, transparent 50%, transparent), linear-gradient(90deg, rgb(33, 150, 243) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+      done()
+    })
+
+    // Set radial-bar to 100% or 1.00
+    vm.fraction = '0.25'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+        .to.equal('linear-gradient(-90deg, rgb(33, 150, 243) 50%, transparent 50%, transparent), linear-gradient(90deg, rgb(33, 150, 243) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+      done()
+    })
+  })
+
+  it('should update contents correctly after changing [strokeColor] props', done => {
+    vm = mountComponent(radialBar, {
+      fraction: '0',
+      overlayText: '00:00',
+      strokeColor: '#A5D173', // rgb(165, 209, 115)
+      trailColor: '#ABABAB' // rgb(171, 171, 171)
+    })
+
+    vm.strokeColor = '#A5D173'
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('.radialBar-progress').style.backgroundImage)
+        .to.equal('linear-gradient(270deg, rgb(171, 171, 171) 50%, transparent 50%, transparent), linear-gradient(270deg, rgb(165, 209, 115) 50%, rgb(171, 171, 171) 50%, rgb(171, 171, 171))')
+      done()
+    })
+  })
+})
---
+++ test/unit/utils.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+
+/**
+ * Destroys the input vue instance
+ * @param  {Object} vm
+ */
+exports.destroyVueInstance = function (vm) {
+  vm.$el &&
+  vm.$el.parentNode &&
+  vm.$el.parentNode.removeChild(vm.$el)
+}
+
+/**
+ * Helper function that mounts and returns the vue instance for testing
+ * @link https://vuejs.org/v2/guide/unit-testing.html#Writing-Testable-Components
+ * @param  {Object}  Component      - component under testing
+ * @param  {Object}  propsData      - props data
+ */
+exports.mountComponent = function (Component, propsData) {
+  const Ctor = Vue.extend(Component)
+  const vm = new Ctor({ propsData }).$mount()
+  // return vm.$el.textContent
+  return vm
+}
