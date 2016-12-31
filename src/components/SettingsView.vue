d57265824060c034e851b07d9621df9d0051287b
Justin Toh <tohjustin@hotmail.com>
2016-12-30 21:48:34 -0800

    Refactored [SettingsView] to use v-model instead of v-on:change

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -5,23 +5,22 @@
     </mu-appbar>
     <mu-list>
       <mu-divider/>
-      <mu-list-item :describeText="workDurationText">
+      <mu-list-item :describeText="myWorkDuration | toMinutes('Work Duration')">
         <mu-icon slot="left" value="access_alarm"/>
-        <mu-slider :step="60" :min="60" :max="3600" :value="workDuration" v-on:change="updateWorkDuration"/>
+        <mu-slider :step="60" :min="60" :max="3600" v-model="myWorkDuration"/>
       </mu-list-item>
       <mu-divider/>
-      <mu-list-item :describeText="breakDurationText">
-        <!-- <mu-icon slot="left" value="snooze"/> -->
+      <mu-list-item :describeText="myBreakDuration | toMinutes('Break Duration')">
         <mu-icon slot="left" value="videogame_asset"/>
-        <mu-slider :step="60" :min="60" :max="3600" :value="breakDuration" v-on:change="updateBreakDuration"/>
+        <mu-slider :step="60" :min="60" :max="3600" v-model="myBreakDuration"/>
       </mu-list-item>
       <mu-divider/>
       <mu-list-item title="Play Alarm Melody">
-        <mu-switch slot="right" :value="allowMelody" v-on:change="updateAllowMelody"/>
+        <mu-switch slot="right" v-model="myAllowMelody"/>
       </mu-list-item>
       <mu-divider/>
-      <mu-list-item title="Vibrate on alarm" :describeText="vibrationNotSupported ? 'Your browser does not support vibration' : 'Your browser supports vibration'">
-        <mu-switch slot="right" :value="vibrationNotSupported ? false : allowVibration" v-on:change="updateAllowVibration" :disabled="vibrationNotSupported"/>
+      <mu-list-item title="Vibrate on alarm" :describeText="vibrationSupported ? 'Your browser supports vibration' : 'Your browser doesn\'t support vibration'">
+        <mu-switch slot="right" v-model="myAllowVibration" :disabled="!vibrationSupported"/>
       </mu-list-item>
       <mu-divider/>
     </mu-list>
@@ -32,50 +31,26 @@
 import Vue from 'vue'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
-// import 'muse-ui/dist/theme-carbon.css' // 使用 carbon 主题
 Vue.use(MuseUI)
 
 export default {
   name: 'settings',
   props: {
-    workDuration: {
-      type: Number,
-      required: true
-    },
-    breakDuration: {
-      type: Number,
-      required: true
-    },
-    allowMelody: {
-      type: Boolean,
-      required: true
-    },
-    allowVibration: {
-      type: Boolean,
-      required: true
-    }
+    workDuration: { type: Number, required: true },
+    breakDuration: { type: Number, required: true },
+    allowMelody: { type: Boolean, required: true },
+    allowVibration: { type: Boolean, required: true }
   },
   data () {
     return {
       myWorkDuration: this.workDuration,
       myBreakDuration: this.breakDuration,
       myAllowMelody: this.allowMelody,
-      myAllowVibration: this.allowVibration
+      myAllowVibration: this.allowVibration,
+      vibrationSupported: false
     }
   },
   methods: {
-    updateWorkDuration (event) {
-      this.myWorkDuration = event
-    },
-    updateBreakDuration (event) {
-      this.myBreakDuration = event
-    },
-    updateAllowMelody (event) {
-      this.myAllowMelody = event
-    },
-    updateAllowVibration (event) {
-      this.myAllowVibration = event
-    },
     backToAppView () {
       let settings = {
         workDuration: this.myWorkDuration,
@@ -86,33 +61,28 @@ export default {
       this.$emit('change', settings)
     }
   },
-  computed: {
-    workDurationText: function () {
-      return `Work Duration (${this.myWorkDuration / 60} min)`
-    },
-    breakDurationText: function () {
-      // let minuteCount = this.breakDuration / 60
-      return `Break Duration (${this.myBreakDuration / 60} min)`
-    },
-    vibrationNotSupported: function () {
-      navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate
-      return !('vibrate' in navigator && navigator.vibrate !== undefined)
+  filters: {
+    toMinutes: function (value, text) {
+      return `${text} (${value / 60} min)`
     }
+  },
+  created: function () {
+    // Check if browser supports HTML5 Vibration API
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate
+    let vibrationSupported = ('vibrate' in navigator && navigator.vibrate !== undefined)
+
+    this.vibrationSupported = vibrationSupported
+    this.myAllowVibration = this.myAllowVibration && vibrationSupported
   }
 }
 </script>
 
-
-<!--  Style cannot be scoped since we're trying to overwrite Muse-UI component's CSS -->
 <style lang="less">
 @import "./../muse-ui-theme/vars.less";
 @import "./../muse-ui-theme/theme-vars.less";
 @import "./../muse-ui-theme/theme.less";
 
-// Overwrite Muse-UI component css
-@primaryColor: @blue500 !important;
-@darkerPrimaryColor: @blue700 !important;
-
+// Overwrite Muse-UI's CSS
 .mu-appbar {
   padding-left: 16px !important;
   padding-right: 16px !important;
@@ -129,8 +99,8 @@ export default {
   padding: 0px !important;
 }
 .mu-item {
-  padding-top: 10px !important;
-  padding-bottom: 10px !important;
+  padding-top: 12px !important;
+  padding-bottom: 12px !important;
 }
 .mu-slider {
   margin-bottom: 0px !important;
--- test/unit/specs/SettingsView.spec.js
+++ test/unit/specs/SettingsView.spec.js
@@ -1,78 +1,59 @@
 import Vue from 'vue'
-import { mountComponent, destroyVueInstance } from '../utils.js'
+import { mountComponent } from '../utils.js'
 import SettingsView from 'src/components/SettingsView.vue'
 
 describe('SettingsView', () => {
-  let vm = null
-
   beforeEach(() => {
-    vm = mountComponent(SettingsView, {
-      workDuration: 1,
-      breakDuration: 1,
-      allowMelody: false,
-      allowVibration: false
-    })
-
     // Alternate way to stub to improve test coverage
     navigator.vibrate = () => {}
   })
 
-  afterEach(() => {
-    destroyVueInstance(vm)
+  describe('Test Hooks', () => {
+    it('[created] checks browser vibration API support correctly (navigator.vibrate !== undefined)', () => {
+      navigator.vibrate = () => {}
+      const vm = mountComponent(SettingsView, {
+        workDuration: 1,
+        breakDuration: 1,
+        allowMelody: false,
+        allowVibration: false
+      })
+
+      expect(vm.myAllowVibration).to.be.false // allowMelody == false
+      expect(vm.vibrationSupported).to.be.true
+    })
+    it('[created] checks browser vibration API support correctly (navigator.vibrate === undefined)', () => {
+      navigator.vibrate = undefined
+      const vm = mountComponent(SettingsView, {
+        workDuration: 1,
+        breakDuration: 1,
+        allowMelody: true,
+        allowVibration: false
+      })
+
+      expect(vm.myAllowVibration).to.be.false
+      expect(vm.vibrationSupported).to.be.false
+    })
   })
 
-  describe('Test Component Methods', () => {
-    it('[updateWorkDuration] updates [myWorkDuration] correctly', () => {
-      vm.updateWorkDuration(1000)
-      expect(vm.myWorkDuration).to.equal(1000)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
-      vm.updateWorkDuration(3600)
-      expect(vm.myWorkDuration).to.equal(3600)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
+  describe('Test Filters', () => {
+    it('[toMinutes] filter transforms the inputs correctly', () => {
+      const vm = mountComponent(SettingsView, {
+        workDuration: 1,
+        breakDuration: 1,
+        allowMelody: false,
+        allowVibration: false
+      })
+
+      expect(vm.$options.filters.toMinutes(6666, 'Test')).to.equal('Test (111.1 min)')
+      expect(vm.$options.filters.toMinutes(600, 'Work Duration')).to.equal('Work Duration (10 min)')
+      expect(vm.$options.filters.toMinutes(0, 'Break Duration')).to.equal('Break Duration (0 min)')
     })
-    it('[updateBreakDuration] updates [myBreakDuration] correctly', () => {
-      vm.updateBreakDuration(1000)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(1000)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
-      vm.updateBreakDuration(3600)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(3600)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
-    })
-    it('[updateAllowMelody] updates [myAllowMelody] correctly', () => {
-      vm.updateAllowMelody(true)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowMelody).to.equal(true)
-      expect(vm.myAllowVibration).to.equal(false)
-      vm.updateAllowMelody(false)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
-    })
-    it('[updateAllowVibration] updates [myAllowVibration] correctly', () => {
-      vm.updateAllowVibration(true)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowMelody).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(true)
-      vm.updateAllowVibration(false)
-      expect(vm.myWorkDuration).to.equal(1)
-      expect(vm.myBreakDuration).to.equal(1)
-      expect(vm.myAllowVibration).to.equal(false)
-      expect(vm.myAllowVibration).to.equal(false)
-    })
+  })
+
+  describe('Test Methods', () => {
     it('[backToAppView] emits [change] event with correct values', () => {
       let spy = sinon.spy()
-      vm = new Vue({
+      const vm = new Vue({
         template: '<div><SettingsView :workDuration="1" :breakDuration="1" :allowMelody="false" :allowVibration="false" v-on:change="callbackFn"></SettingsView></div>',
         components: { SettingsView },
         methods: {
@@ -80,6 +61,7 @@ describe('SettingsView', () => {
         }
       }).$mount()
       vm.$children[0].backToAppView()
+
       expect(spy).to.have.been.calledOnce
       expect(spy).to.have.been.calledWith({ workDuration: 1, breakDuration: 1, allowMelody: false, allowVibration: false })
     })
