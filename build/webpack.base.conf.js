f7d6e05935c88e27aff2c87203e320507b779b84
Justin Toh <tohjustin@hotmail.com>
2017-01-02 15:33:02 -0800

    Modified how sw.js & manifest.json is loaded

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 var env = process.env.NODE_ENV
 // check env & config/index.js to decide whether to enable CSS source maps for the
@@ -90,5 +91,11 @@ module.exports = {
         browsers: ['last 2 versions']
       })
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      // { from: './src/sw.js' },
+      { from: './src/manifest.json' },
+    ]),
+  ]
 }
