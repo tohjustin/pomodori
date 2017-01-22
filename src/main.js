265b8a3e541c610a47abbb62f96b3508ae35869a
Justin Toh <tohjustin@hotmail.com>
2017-01-21 23:11:07 -0800

    Added Helper modules to interact with service worker & Notifications API

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    // navigator.serviceWorker.register(swURL).then(function (registration) {
-    navigator.serviceWorker.register('./sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope)
-    }).catch(function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err)
-    })
-  })
-} else {
-  console.log('Service worker not supported')
-}
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import notificationHelper from './js/notificationHelper'
+import serviceWorkerHelper from './js/serviceWorkerHelper'
+
+serviceWorkerHelper.register() // Register Service Worker
+notificationHelper.requestPermission() // Request for permission to send local notifications
 
 /* eslint-disable no-new */
 new Vue({
