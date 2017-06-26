a864d50e2c4ca19285397acf2b4f507bf655edbb
Justin Toh <tohjustin@hotmail.com>
2017-06-25 18:52:54 -0700

    test: Fix utils.js exporting errors

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -4,9 +4,9 @@ import Vue from 'vue';
  * Destroys the input vue instance
  * @param  {Object} vm
  */
-const destroyVueInstance = (vm) => {
+export function destroyVueInstance(vm) {
   vm.$destroy();
-};
+}
 
 /**
  * Helper function that mounts and returns the vue instance for testing
@@ -14,16 +14,11 @@ const destroyVueInstance = (vm) => {
  * @param  {Object}  Component      - component under testing
  * @param  {Object}  propsData      - props data
  */
-const mountComponent = (Component, propsData) => {
+export function mountComponent(Component, propsData) {
   const Ctor = Vue.extend(Component);
   const vm = new Ctor({
     propsData,
   }).$mount();
 
   return vm;
-};
-
-export default {
-  destroyVueInstance,
-  mountComponent,
-};
+}
