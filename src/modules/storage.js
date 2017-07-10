f45f8aaa211f5604221213af2c76b406e279f559
Justin Toh <tohjustin@hotmail.com>
2017-07-09 23:23:53 -0700

    storage: Remove console logs

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -17,28 +17,16 @@ const loadSettings = () => (
   new Promise((resolve, reject) => {
     localforage.getItem(STORAGE_KEY)
       .then((value) => {
-        console.log('User settings loaded succesfully: ', value);
         resolve(value);
       })
       .catch((err) => {
-        console.log('User settings loaded unsuccesfully: ', err);
         reject(err);
       });
   })
 );
 
 const saveSettings = (settings) => {
-  localforage.setItem(STORAGE_KEY, settings)
-    .then(() => {
-      console.log('Saving user settings...');
-      return localforage.getItem(STORAGE_KEY);
-    })
-    .then((value) => {
-      console.log('User settings saved succesfully: ', value);
-    })
-    .catch((err) => {
-      console.log('User settings saved unsuccesfully: ', err);
-    });
+  localforage.setItem(STORAGE_KEY, settings);
 };
 
 export default {
